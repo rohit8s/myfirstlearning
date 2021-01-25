cf3c03cc813732a0a89fde9f96dd5c391f7a611e
Rohit Shrivastava <69757655+rohit8s@users.noreply.github.com>
2021-01-25 14:16:02 -0500

    Palindrome

--- src/Solution.java
+++ src/Solution.java
@@ -6,18 +6,29 @@ method 2 : takes two string as input and return boolean
  exp : "tarun" "nurat" return true || "nama" "try" return false
  */
 public class Solution {
+
     public static void main(String[] args) {
-
-        String S1 = "Method 1";
-        String S2 = "Method 2";
-
-        int length = S1.length();
-        SampleString sampleString = new SampleString();
-        System.out.println("Length of a String is: " + sampleString.getStringLength(S1));
-        System.out.println("Length of a String is: " + sampleString.getStringLength(S2));
+        Solution solution = new Solution();
+        char[] arr = new char[10];
+        char ch = 'A';
+        for (int i = 0; i < 10; i++) {
+            arr[i] = ch++;
+        }
+        System.out.println("charArray to String conversion : " + solution.getStringFromArray(arr));
+        //method 2
+        System.out.println("checking whether two strings are palindrome to each other : " + solution.checkPalindromeInString("tarun", "nurat"));
     }
-    public int getStringLength(String str)
-    {
-        return str.length();
+
+    public String getStringFromArray(char... arr) {
+        char c = arr[arr.length];
+        return String.valueOf(c);
     }
+
+    public boolean checkPalindromeInString(String s1, String s2) {
+        return Boolean.parseBoolean("palindrome");
+
+        //ABCDEFGHIJ
+        //true
+    }
+
 }
